1659fc6ec5f67751ba19db050e453122c436e97f
Paultarso97 <paulo.detarso1997@gmail.com>
2025-02-14 15:11:53 -0300

    Add files via upload

---
+++ atividade_hotel.py
@@ -0,0 +1,37 @@
+clientes = []
+cliente1 = "paulo"
+idade1 = 27
+qtd_dias1 = 10
+quarto1 = "simples"
+
+if quarto1 == "simples":
+    print(f"O cliente {cliente1}, de {idade1} anos, o valor pago no quarto tipo {quarto1}, por {qtd_dias1} será de {100 * qtd_dias1}")
+elif quarto1 == "duplo":
+    print(f"O cliente {cliente1}, de {idade1} anos, o valor pago no quarto tipo {quarto1}, por {qtd_dias1} será de {150 * qtd_dias1}")
+elif quarto1 == "luxo":
+    print(f"O cliente {cliente1}, de {idade1} anos, o valor pago no quarto tipo {quarto1}, por {qtd_dias1} será de {250 * qtd_dias1}")
+
+cliente2 = "gustavo"
+idade2 = 27
+qtd_dias2 = 10
+quarto2 = "luxo"
+
+if quarto2 == "simples":
+    print(f"O cliente {cliente2}, de {idade2} anos, o valor pago no quarto tipo {quarto2}, por {qtd_dias2} será de {100 * qtd_dias2}")
+elif quarto2 == "duplo":
+    print(f"O cliente {cliente2}, de {idade2} anos, o valor pago no quarto tipo {quarto2}, por {qtd_dias2} será de {150 * qtd_dias2}")
+elif quarto2 == "luxo":
+    print(f"O cliente {cliente2}, de {idade2} anos, o valor pago no quarto tipo {quarto2}, por {qtd_dias2} será de {250 * qtd_dias2}")
+
+cliente3 = "gustavo"
+idade3 = 56
+qtd_dias3 = 10
+quarto3 = "luxo"
+
+if quarto3 == "simples":
+    print(f"O cliente {cliente3}, de {idade3} anos, o valor pago no quarto tipo {quarto3}, por {qtd_dias3} será de {100 * qtd_dias3}")
+elif quarto3 == "duplo":
+    print(f"O cliente {cliente3}, de {idade3} anos, o valor pago no quarto tipo {quarto3}, por {qtd_dias3} será de {150 * qtd_dias3}")
+elif quarto3 == "luxo":
+    print(f"O cliente {cliente3}, de {idade3} anos, o valor pago no quarto tipo {quarto3}, por {qtd_dias3} será de {250 * qtd_dias3}")
+    
